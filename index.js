fd7f8e45e49c1d26aaeffaffe42fc71810e8cbd9
Wojtek Stryszewski <stryszewski@gmail.com>
2018-05-01 11:38:55 +0200

    add google oauth config

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const googleClientID = require('./config').googleClientID;
+const googleClientSecret = require('./config').googleClientSecret;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+passport.use(new GoogleStrategy());
 
 app.listen(PORT, () => console.log(`Server is listen on http://localhost:${PORT}`));
